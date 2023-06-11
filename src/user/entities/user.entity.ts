c5043e69abeeea924e9d36b194acdedfdc07c1c2
Efren <50155831+EfrenAS@users.noreply.github.com>
2023-06-10 23:34:54 -0500

    Make to Relationship entities for database

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,30 @@
+import { Column, Entity, OneToOne } from "typeorm";
+import { BaseEntity } from "../../config/base.entity";
+import { CustomerEntity } from "../../customer/entities/customer.entity";
+
+@Entity({ name: "user" }) //Initiliazed the User Entity
+export class UserEntity extends BaseEntity {
+  @Column({ length: 20 })
+  name!: string;
+
+  @Column()
+  lastname!: string;
+
+  @Column()
+  username!: string;
+
+  @Column()
+  email!: string;
+
+  @Column()
+  password!: string;
+
+  @Column()
+  city!: string;
+
+  @Column()
+  province!: string;
+
+  @OneToOne(() => CustomerEntity, (customer) => customer.user)
+  customer!: CustomerEntity;
+}
